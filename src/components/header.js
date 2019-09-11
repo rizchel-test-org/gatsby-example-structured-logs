ffc78a57e10c9dabd865399ab8c7ca83e51f1563
abhiaiyer91 <abhiaiyer91@gmail.com>
2019-09-11 16:51:30 -0700

    bump

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useEffect } from "react"
 
 const Header = ({ siteTitle }) => (
   <header
