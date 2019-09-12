5dd4f661e24cf1e6a909f593e817667e8fc70b24
Dustin Schau <dustinschau@gmail.com>
2019-09-11 17:05:00 -0700

    chore: add a warning

---
+++ src/components/dingus.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+function Dingus() {
+  return (
+    <p>Just to show a warning&hellip; move along now.</p>
+  )
+}
+
+export const dingusQuery = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
+
+export default Dingus
